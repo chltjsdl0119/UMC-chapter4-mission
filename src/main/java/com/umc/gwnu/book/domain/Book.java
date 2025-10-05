76b30fe6e6e281d9fc486ca36de5546a1d793a45
Seungwon-Choi <bestwondev@gmail.com>
2025-10-05 22:05:06 +0900

    feat: Book 엔티티 클래스 추가

---
+++ src/main/java/com/umc/gwnu/book/domain/Book.java
@@ -0,0 +1,37 @@
+package com.umc.gwnu.book.domain;
+
+import com.umc.gwnu.global.domain.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class Book extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_category_id")
+    private BookCategory bookCategory;
+
+    private String title;
+
+    private String description;
+}
